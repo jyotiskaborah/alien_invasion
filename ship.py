2c1501db37c5aba3449fc7686311493bffb6fde1
Jyotiska Borah <jyotiskaborah@gmail.com>
2024-09-01 19:49:03 +0530

    firstPush

---
+++ ship.py
@@ -0,0 +1,39 @@
+import pygame
+
+class Ship:
+    """A class to manage ship."""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set the starting position."""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start new ship at bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flags, start with a ship that's not moving.
+        self.moving_right = False
+        self.moving_left = False
+    
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+    
+    def update(self):
+        """Update the ship's position based on movement flags."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        
+        #update rect object from self.x.
+        self.rect.x = self.x
+
